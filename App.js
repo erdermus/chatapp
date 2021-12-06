87c0ddea454eedd62e6c76ea2f3c4013df6a43a2
erdermus <ytvufoxer@gmail.com>
2021-12-06 19:06:30 +0100

    handle user-view

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { useAssets } from 'expo-asset';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase';
 
 function App() {
+  const [currentUser , setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, user => {
+      setLoading(false);
+      if(user) {
+        setCurrentUser(user);
+      }
+    });
+    return () => unsubscribe();
+  }, [])
+
+  if(loading){
+    return <Text>loading ...</Text>
+  }
+
   return (
     <View style={styles.container}>
-      <Text>CHAT APP</Text>
+      <Text>{JSON.stringify(currentUser)}</Text>
+      <Text>TEST</Text>
       <StatusBar style="auto" />
     </View>
   );
