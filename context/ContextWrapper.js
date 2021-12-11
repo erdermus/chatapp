e1d6dae998aded31027ac6513ecec991c4426285
erdermus <ytvufoxer@gmail.com>
2021-12-11 14:49:31 +0100

    add context for theme

---
+++ context/ContextWrapper.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import Context from "./Context";
+import { theme } from "../utils";
+
+export default function ContextWrapper(props) {
+  const [rooms, setRooms] = useState([]);
+  const [unfilteredRooms, setUnfilteredRooms] = useState([]);
+  return (
+    // <Context.Provider value={{ theme, rooms, setRooms, unfilteredRooms, setUnfilteredRooms }}>
+    <Context.Provider value={{ theme }}>
+      {props.children}
+    </Context.Provider>
+  );
+}
